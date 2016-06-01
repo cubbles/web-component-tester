c65bf875e72a3206dcf9548ca505c4985cc4bd85
Judit Ross <jtrs@incowia.com>
2016-06-01 15:27:10 +0200

    PLAT-515: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## web-component.testert-component-tester
+
+<hr/>
+Note This page is generated based on the markdown-syntax. For details see <http://daringfireball.net/projects/markdown/syntax/>
---
+++ webpackages/web-component-tester-4.2.2/mocha-config/js/mocha-config.js
@@ -0,0 +1,10 @@
+/* global mocha,chai */
+/* exported assert,should,expect */
+'use strict';
+mocha.setup({ ui: 'bdd' });
+// eslint-disable-next-line no-unused-vars
+var assert = chai.assert;
+// eslint-disable-next-line no-unused-vars
+var expect = chai.expect;
+// Note that should has to be executed
+var should = chai.should();
